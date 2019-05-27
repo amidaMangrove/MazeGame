434c317705997b1081de6bfaad2eb9727e66b82c
s-sawaguchi <sawaguchi0604@gmail.com>
2019-05-27 23:15:31 +0900

    Add Assets

---
+++ Assets/Script/Goal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    [SerializeField] GameObject goalEffect;
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(goalEffect, new Vector3(0, 0, 0), Quaternion.Euler(-90.0f,0,0));
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item") {
+            Debug.Log("アイテムに衝突！");
+
+            Destroy(other.gameObject);
+        }
+    }
+}
